7c32aba5735b3f8f361a3e4cb70d7d2ff01581b3
Donald Pakkies <donaldpakkies@gmail.com>
2019-02-16 00:31:06 +0200

    Feat: updated the default docs layout
    Add versions

--- resources/views/app/docs/default.medusa.php
+++ resources/views/app/docs/default.medusa.php
@@ -14,12 +14,74 @@
   {% foreach config('docsify.sheets') as $sheet %}
     <link rel="stylesheet" href="{{ $sheet }}">
   {% endforeach %}
+
+  <style>
+    body {
+      font-size: 13px;
+      line-height: 1.9;
+    }
+
+    .select-style {
+      padding: 0;
+      margin: 0;
+      border: 1px solid #ccc;
+      width: 55px;
+      border-radius: 3px;
+      background-color: #fff;
+
+      background: #fff url("http://www.scottgood.com/jsg/blog.nsf/images/arrowdown.gif") no-repeat 90% 50%;
+    }
+
+  .select-style select {
+      padding: 5px 8px;
+      width: 55px;
+      border: none;
+      box-shadow: none;
+      background-color: transparent;
+      background-image: none;
+      -webkit-appearance: none;
+        -moz-appearance: none;
+              appearance: none;
+    }
+
+    .select-style select:focus {
+      outline: none;
+    }
+  </style>
+
 </head>
 <body>
+  <nav id="nav" data-versions="{{ json_encode($versions) }}">
+    <ul>
+      <li><a href="{{ url('/', true) }}">Home</a></li>
+      <li>
+        <div class="select-style">
+          <select id="versions" onclick="select(this);">
+            {% foreach $versions as $version %}
+              <option id="switch" data-title="{{ $version['title'] }}" {{ $version['selected'] ? 'selected' : '' }}>{{ $version['title'] }}</option>
+            {% endforeach %}
+          </select>
+        </div>
+      </li>
+    </ul>
+  </nav>
   <div id="app"></div>
 
   <script>
     window.$docsify = {% configToJson('docsify.config') %}
+
+    let versions = JSON.parse(document.getElementById('nav').getAttribute('data-versions'));
+
+    versions.forEach(function(route, i) {
+      if (window.location.hash == route.hash) {
+        document.getElementById('versions').selectedIndex = i;
+      }
+    });
+
+    function select(e) {
+      window.location.href = "{{ url('/docs/#/', true) }}" + e.value + "/";
+    }
+
   </script>
 
   {% foreach config('docsify.scripts') as $script %}
